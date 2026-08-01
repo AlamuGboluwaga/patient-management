031d8d6453de3763178147a02659d981c46d4a2e
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2026-08-01 05:28:57 +0100

    initial commit

---
+++ patient-service/src/main/java/com/elroi/patientservice/PatientServiceApplication.java
@@ -0,0 +1,26 @@
+package com.elroi.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+
+//        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+//
+//        dotenv.entries().forEach(entry -> {
+//            String key = entry.getKey();
+//            String value = entry.getValue();
+//            if (key.startsWith("SPRING_DATASOURCE_")) {
+//                key =
+//                        "spring.datasource." +
+//                                key.substring("SPRING_DATASOURCE_".length()).toLowerCase();
+//            }
+//            System.setProperty(key, value);
+//        });
+        SpringApplication.run(PatientServiceApplication.class, args);
+	}
+
+}
